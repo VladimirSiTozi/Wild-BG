3ae96b30d0f6b62ab91c0cb89e1947855708199a
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-11-13 13:45:08 +0200

    Test repo

--- wildBg/settings.py
+++ wildBg/settings.py
@@ -33,12 +33,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Built-in Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
 
     # My apps
     "wildBg.accounts.apps.AccountsConfig",
@@ -140,3 +143,5 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
