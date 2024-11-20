3de6557cfe9513869c2a5e4504332d0030027acb
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-11-20 22:58:03 +0200

    add: footer template, add: Post, PostComment, PostComment models

--- wildBg/post/models.py
+++ wildBg/post/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+from wildBg.accounts.models import AppUser
+from wildBg.landmark.models import Landmark
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        AppUser,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+
+    location = models.ForeignKey(
+        Landmark,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    post_image = models.ImageField(
+        upload_to='posts/',
+        null=True,
+        blank=True,
+    )
+
+    description = models.TextField()
+
+    tagged_people = models.ManyToManyField(
+        AppUser,
+        related_name='tagged_posts',
+        blank=True,
+    )
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+
+    author = models.ForeignKey(
+        AppUser,
+        on_delete=models.CASCADE,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'Comment by {self.author.firs_name} on {self.created_at.strftime("%Y-%m-%d")}'
+
+
+class ReplyPostComment(models.Model):
+    comment = models.ForeignKey(
+        PostComment,
+        on_delete=models.CASCADE,
+        related_name='replies',
+    )
+
+    author = models.ForeignKey(
+        AppUser,
+        on_delete=models.CASCADE,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'Reply by {self.author.first_name} on {self.created_at.strftime("%Y-%m-%d")}'
